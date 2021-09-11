1be5881b2edcd9022902faa9f5bbf35a695f6bbd
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-09-11 19:09:47 +0200

    Final touches

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Button.css';
 
+// For each button
 const Button = (props) => {
 	return (
 		<div className={`button ${props.imp === 'secondary' ? 'button__white' : ''}`}>
