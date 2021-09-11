b2b2a830fd87a34f3a7babf5989461d7b907a8e8
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-09-11 12:24:41 +0200

    Imported the expand icon and added background image

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Button from './Button';
+
+import './Body.css'
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const Body = (props) => {
 	return (
-		<div className="body">
+		<div className="body" style={{
+			backgroundImage: `url(${props.bcgImg})`
+		}}>
 			<div className="body_container">
 			
 				<div className="body_upper">
@@ -14,8 +21,20 @@ const Body = (props) => {
 				
 				<div className="body_lower">
 					<div className="buttons">
+						<Button imp='primary' text={props.leftBtn} Url={props.leftBtnUrl} />
 						
+						{/*Render the second button only if twoBtns is true i.e. if two buttons exist in that component*/}
+						{props.twoBtns && (
+							<Button imp='secondary' text={props.rightBtn} Url={props.rightBtnUrl} />
+						)}
 					</div>
+					
+					{props.first && (
+						<div className="expand">
+							<ExpandMoreIcon />
+						</div>
+					)}					
+					
 				</div>
 				
 			</div>
